268f2e88e0974571b0787b7fe7bf9793fef0da32
Ian Zhang <izhang@redhat.com>
2020-10-26 16:46:06 -0400

    a
    Signed-off-by: Ian Zhang <izhang@redhat.com>

--- build/test.sh
+++ build/test.sh
@@ -1,9 +1,7 @@
 #! /bin/bash
 # Set KUBECONFIG environment variable.
-export KUBECONFIG="$(kind get kubeconfig)"
 kind get kubeconfig > kindconfig
-kind get kubeconfig > $HOME/.kube/config
 sleep 30
 kubectl get po -A --kubeconfig kindconfig
-kubectl get ns -A
+kubectl get ns -A --Kubeconfig kindconfig
 
