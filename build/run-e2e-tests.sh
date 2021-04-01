59cca3a4dda57dfd1b7ddd1d6d47fe7eafcddac6
Ian Zhang <izhang@redhat.com>
2021-04-01 19:47:18 -0400

    use the new server interface

--- build/run-e2e-tests.sh
+++ build/run-e2e-tests.sh
@@ -20,33 +20,33 @@ echo
 export GO111MODULE=on
 
 if [ "$TRAVIS_BUILD" != 1 ]; then
-    echo "Build is on $RUN_ON"
+	echo "Build is on $RUN_ON"
 
-    # Download and install kubectl
-    echo -e "\nGet kubectl binary\n"
-    PLATFORM=`uname -s | awk '{print tolower($0)}'`
-    if [ "`which kubectl`" ]; then
-        echo "kubectl PATH is `which kubectl`"
-    else
-        mkdir -p $(pwd)/bin
-        curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/$PLATFORM/amd64/kubectl && mv kubectl $(pwd)/bin/ && chmod +x $(pwd)/bin/kubectl
-        export PATH=$PATH:$(pwd)/bin
-        echo "kubectl PATH is `which kubectl`"
-    fi
+	# Download and install kubectl
+	echo -e "\nGet kubectl binary\n"
+	PLATFORM=$(uname -s | awk '{print tolower($0)}')
+	if [ "$(which kubectl)" ]; then
+		echo "kubectl PATH is $(which kubectl)"
+	else
+		mkdir -p $(pwd)/bin
+		curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/$PLATFORM/amd64/kubectl && mv kubectl $(pwd)/bin/ && chmod +x $(pwd)/bin/kubectl
+		export PATH=$PATH:$(pwd)/bin
+		echo "kubectl PATH is $(which kubectl)"
+	fi
 
-    echo -e "\nDownload and install KinD\n"
-    go get sigs.k8s.io/kind@v0.9.0
+	echo -e "\nDownload and install KinD\n"
+	go get sigs.k8s.io/kind@v0.9.0
 
 fi
 
 kind delete cluster
 if [ $? != 0 ]; then
-        exit $?;
+	exit $?
 fi
 
 kind create cluster --image=kindest/node:v1.19.1
 if [ $? != 0 ]; then
-        exit $?;
+	exit $?
 fi
 
 sleep 15
@@ -55,122 +55,119 @@ if [ ! -d "default-kubeconfigs" ]; then
 	mkdir default-kubeconfigs
 fi
 
-
 kind get kubeconfig > default-kubeconfigs/hub
 
-setup_application_operator(){
-    echo "Clone the application repo"
-    echo
-    if [ ! -d "multicloud-operators-application" ]; then
-        git clone https://github.com/open-cluster-management/multicloud-operators-application.git
-    fi
+setup_application_operator() {
+	echo "Clone the application repo"
+	echo
+	if [ ! -d "multicloud-operators-application" ]; then
+		git clone https://github.com/open-cluster-management/multicloud-operators-application.git
+	fi
 
-    kubectl apply -f multicloud-operators-application/deploy/crds
+	kubectl apply -f multicloud-operators-application/deploy/crds
 }
 
-setup_channel_operator(){
-    echo "Clone the channel repo"
-    echo
-    if [ ! -d "multicloud-operators-channel" ]; then
-        git clone https://github.com/open-cluster-management/multicloud-operators-channel.git
-    fi
+setup_channel_operator() {
+	echo "Clone the channel repo"
+	echo
+	if [ ! -d "multicloud-operators-channel" ]; then
+		git clone https://github.com/open-cluster-management/multicloud-operators-channel.git
+	fi
 
 	sed -i -e "s|image: .*$|image: quay.io/open-cluster-management/multicluster-operators-channel:community-${COMPONENT_VERSION}|" multicloud-operators-channel/deploy/standalone/operator.yaml
 
-    kubectl apply -f multicloud-operators-channel/deploy/crds
-    kubectl apply -f multicloud-operators-channel/deploy/standalone
+	kubectl apply -f multicloud-operators-channel/deploy/crds
+	kubectl apply -f multicloud-operators-channel/deploy/standalone
 
-    kubectl rollout status deployment/multicluster-operators-channel
-    if [ $? != 0 ]; then
-        echo "failed to deploy the channel operator"
-        exit $?;
-    fi
+	kubectl rollout status deployment/multicluster-operators-channel
+	if [ $? != 0 ]; then
+		echo "failed to deploy the channel operator"
+		exit $?
+	fi
 }
 
-setup_subscription_operator(){
-    echo "Clone the subscription repo"
-    echo
-    if [ ! -d "multicloud-operators-subscription" ]; then
-        git clone https://github.com/open-cluster-management/multicloud-operators-subscription.git
-    fi
+setup_subscription_operator() {
+	echo "Clone the subscription repo"
+	echo
+	if [ ! -d "multicloud-operators-subscription" ]; then
+		git clone https://github.com/open-cluster-management/multicloud-operators-subscription.git
+	fi
 
-    kubectl apply -f multicloud-operators-subscription/deploy/common
-    sleep 5
+	kubectl apply -f multicloud-operators-subscription/deploy/common
+	sleep 5
 
 	echo "before sed $COMPONENT_VERSION"
-    sed -i -e "s|image: .*$|image: quay.io/open-cluster-management/multicluster-operators-subscription:community-$COMPONENT_VERSION|" multicloud-operators-subscription/deploy/standalone/operator.yaml
+	sed -i -e "s|image: .*$|image: quay.io/open-cluster-management/multicluster-operators-subscription:community-$COMPONENT_VERSION|" multicloud-operators-subscription/deploy/standalone/operator.yaml
 
-    kubectl apply -f multicloud-operators-subscription/deploy/standalone
+	kubectl apply -f multicloud-operators-subscription/deploy/standalone
 
-    kubectl rollout status deployment/multicluster-operators-subscription -n multicluster-operators
-    if [ $? != 0 ]; then
-        echo "failed to deploy the subscription operator"
-        exit $?;
-    fi
+	kubectl rollout status deployment/multicluster-operators-subscription -n multicluster-operators
+	if [ $? != 0 ]; then
+		echo "failed to deploy the subscription operator"
+		exit $?
+	fi
 }
 
-setup_placementrule_operator(){
-    echo "Clone the placementrule repo"
-    echo
-    if [ ! -d "multicloud-operators-placementrule" ]; then
-        git clone https://github.com/open-cluster-management/multicloud-operators-placementrule.git
-    fi
+setup_placementrule_operator() {
+	echo "Clone the placementrule repo"
+	echo
+	if [ ! -d "multicloud-operators-placementrule" ]; then
+		git clone https://github.com/open-cluster-management/multicloud-operators-placementrule.git
+	fi
 
-    kubectl apply -f https://raw.githubusercontent.com/open-cluster-management/multicloud-operators-placementrule/master/deploy/crds/apps.open-cluster-management.io_placementrules_crd.yaml
+	kubectl apply -f https://raw.githubusercontent.com/open-cluster-management/multicloud-operators-placementrule/master/deploy/crds/apps.open-cluster-management.io_placementrules_crd.yaml
 }
 
-setup_helmrelease_operator(){
-    echo "Clone the helmrelease repo"
-    echo
-    if [ ! -d "multicloud-operators-subscription-release" ]; then
-        git clone https://github.com/open-cluster-management/multicloud-operators-subscription-release.git
-    fi
+setup_helmrelease_operator() {
+	echo "Clone the helmrelease repo"
+	echo
+	if [ ! -d "multicloud-operators-subscription-release" ]; then
+		git clone https://github.com/open-cluster-management/multicloud-operators-subscription-release.git
+	fi
 
-    sed -i -e "s|image: .*$|image: quay.io/open-cluster-management/multicluster-operators-subscription-release:community-$COMPONENT_VERSION|" multicloud-operators-subscription-release/deploy/operator.yaml
+	sed -i -e "s|image: .*$|image: quay.io/open-cluster-management/multicluster-operators-subscription-release:community-$COMPONENT_VERSION|" multicloud-operators-subscription-release/deploy/operator.yaml
 
-    kubectl apply -f multicloud-operators-subscription-release/deploy/crds
-    kubectl apply -f multicloud-operators-subscription-release/deploy
+	kubectl apply -f multicloud-operators-subscription-release/deploy/crds
+	kubectl apply -f multicloud-operators-subscription-release/deploy
 
-    kubectl rollout status deployment/multicluster-operators-subscription-release
-    if [ $? != 0 ]; then
-        echo "failed to deploy the subscription operator"
-        exit $?;
-    fi
+	kubectl rollout status deployment/multicluster-operators-subscription-release
+	if [ $? != 0 ]; then
+		echo "failed to deploy the subscription operator"
+		exit $?
+	fi
 
-    echo -e "\nApply the Apache service with basic auth and helm chart\n"
-    kubectl apply -f apache-basic-auth/apache-basic-auth-service.yaml
+	echo -e "\nApply the Apache service with basic auth and helm chart\n"
+	kubectl apply -f apache-basic-auth/apache-basic-auth-service.yaml
 }
 
-setup_operators(){
+setup_operators() {
 	kubectl apply -f https://raw.githubusercontent.com/open-cluster-management/multicloud-operators-placementrule/master/hack/test/crds/clusters.open-cluster-management.io_managedclusters.crd.yaml
 
-    setup_application_operator
+	setup_application_operator
 	setup_placementrule_operator
 
 	setup_subscription_operator
-    setup_channel_operator
-    setup_helmrelease_operator
+	setup_channel_operator
+	setup_helmrelease_operator
 
+	if [ "$TRAVIS_BUILD" != 1 ]; then
+		sleep 90
+	fi
 
-    if [ "$TRAVIS_BUILD" != 1 ]; then
-        sleep 90
-    fi
-
-   	echo -e "\nRunning images\n"
+	echo -e "\nRunning images\n"
 	kubectl get deploy -A -o jsonpath='{.items[*].spec.template.spec.containers[*].image}' | xargs -n1 echo
 
-    echo -e "\nPod status\n"
+	echo -e "\nPod status\n"
 
 	kubectl get po -A
 }
 
-function cleanup()
-{
+function cleanup() {
 
 	docker kill apache-basic-auth-container || true
 	docker rm apache-basic-auth-container || true
 
-  	echo -e "\nPod status\n"
+	echo -e "\nPod status\n"
 
 	kubectl get po -A
 
@@ -179,27 +176,24 @@ function cleanup()
 	kubectl get ValidatingWebhookConfiguration -n default
 }
 
-start_up_private_helm(){
+start_up_private_helm() {
 	echo "start up private helm registery"
 	docker build -t ${APACHE_BASIC_AUTH_IMAGE} -f apache-basic-auth/Dockerfile .
-	docker run -d --name ${APACHE_BASIC_AUTH_CONTAINER}  -p 8080:8080 ${APACHE_BASIC_AUTH_IMAGE}
+	docker run -d --name ${APACHE_BASIC_AUTH_CONTAINER} -p 8080:8080 ${APACHE_BASIC_AUTH_IMAGE}
 }
-
 
 start_up_private_helm
 setup_operators
-
 
 trap cleanup EXIT
 
 export KUBE_DIR="../../default-kubeconfigs"
 # echo "Process the canary test cases"
-# go test -v ./client/canary/...
 
+go test -v ./client/canary/...
 
 # The default go test time is 10 minutes, we need to increase test time after adding more test cases
 echo "Process the API test cases"
 go test -v ./client/e2e_client/... -timeout 30m
 
 exit 0
-
