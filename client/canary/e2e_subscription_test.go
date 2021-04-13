cf7306120bd79e6a1a188b5a4439b6fca3952314
rokej <roke@redhat.com>
2021-04-13 09:28:19 -0400

    Fix squad name

--- client/canary/e2e_subscription_test.go
+++ client/canary/e2e_subscription_test.go
@@ -6,10 +6,10 @@ import (
 )
 
 var _ = Describe("e2e-git-subscription-test", func() {
-	It("[P1][Sev1][app-life-cycle] Test subscription with Git release tag", func() {
+	It("[P1][Sev1][app-lifecycle] Test subscription with Git release tag", func() {
 		Eventually(func() error { return DefaultRunner.Run("sub-003") }, 5*pullInterval, pullInterval).Should(Succeed())
 	})
-	It("[P1][Sev1][app-life-cycle] Test subscription with Git commit", func() {
+	It("[P1][Sev1][app-lifecycle] Test subscription with Git commit", func() {
 		Eventually(func() error { return DefaultRunner.Run("sub-004") }, 5*pullInterval, pullInterval).Should(Succeed())
 	})
 })
