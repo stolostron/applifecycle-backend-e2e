2f5871791550af4d3e31a3f270db425a4e571afc
Xiangjing Li <xiangli@redhat.com>
2021-08-23 13:46:51 -0400

    enable Canary test in 2.4
    Signed-off-by: Xiangjing Li <xiangli@redhat.com>

--- client/canary/e2e_canary_git_custom_cert_test.go
+++ client/canary/e2e_canary_git_custom_cert_test.go
@@ -3,18 +3,19 @@ package client_test
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/open-cluster-management/applifecycle-backend-e2e/pkg"
 )
 
 var _ = Describe("e2e-server", func() {
 	It("[P1][Sev1][app-lifecycle] Test subscribing to Git repo with custom certificate", func() {
-		/*ret := pkg.RunCMD("./scripts/gitServer/install.sh")
+		ret := pkg.RunCMD("./scripts/gitServer/install.sh")
 		Expect(ret).To(Equal(0))
 
 		ret = pkg.RunCMD("./scripts/git_custom_certs.sh")
 		Expect(ret).To(Equal(0))
 
-		ret = pkg.RunCMD("./scripts/gitServer/uninstall.sh")*/
-		ret := 0
+		ret = pkg.RunCMD("./scripts/gitServer/uninstall.sh")
+		//ret := 0
 		Expect(ret).To(Equal(0))
 	})
 })
