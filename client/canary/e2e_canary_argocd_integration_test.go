bfbd1333810d885516802a155815717fa3d6035e
Xiangjing Li <55890329+xiangjingli@users.noreply.github.com>
2021-08-23 15:12:00 -0400

    enable Canary test in 2.4 (#132)
    Signed-off-by: Xiangjing Li <xiangli@redhat.com>

--- client/canary/e2e_canary_argocd_integration_test.go
+++ client/canary/e2e_canary_argocd_integration_test.go
@@ -3,12 +3,13 @@ package client_test
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/open-cluster-management/applifecycle-backend-e2e/pkg"
 )
 
 var _ = Describe("e2e-server", func() {
 	It("[P1][Sev1][app-lifecycle] Test argocd integration", func() {
-		//ret := pkg.RunCMD("./scripts/gitopscluster_test.sh")
-		ret := 0
+		ret := pkg.RunCMD("./scripts/gitopscluster_test.sh")
+		//ret := 0
 		Expect(ret).To(Equal(0))
 	})
 })
--- client/canary/e2e_canary_helmrepo_basicath_test.go
+++ client/canary/e2e_canary_helmrepo_basicath_test.go
@@ -3,12 +3,13 @@ package client_test
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/open-cluster-management/applifecycle-backend-e2e/pkg"
 )
 
 var _ = Describe("RHACM4K-2350: e2e-server", func() {
 	It("[P1][Sev1][app-lifecycle] Test subscribing to Helm repo with basic authentication", func() {
-		//ret := pkg.RunCMD("./scripts/helmrepo_basicath.sh")
-		ret := 0
+		ret := pkg.RunCMD("./scripts/helmrepo_basicath.sh")
+		//ret := 0
 		Expect(ret).To(Equal(0))
 	})
 })
