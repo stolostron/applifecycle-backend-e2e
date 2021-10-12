f35460e180ea0eb039715ffc68fede5de2988030
Roke Jung <roke@redhat.com>
2021-10-12 13:34:33 -0400

    move test
    Signed-off-by: Roke Jung <roke@redhat.com>

---
+++ client/canary/e2e_canary_subscription_admin_test.go
@@ -0,0 +1,14 @@
+package client_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/open-cluster-management/applifecycle-backend-e2e/pkg"
+)
+
+var _ = Describe("e2e-subscription-admin-test", func() {
+	It("[P1][Sev1][app-lifecycle] Test nested subscriptions with subscription admin", func() {
+		ret := pkg.RunCMD("./scripts/subscriptionAdmin/test.sh")
+		Expect(ret).To(Equal(0))
+	})
+})
