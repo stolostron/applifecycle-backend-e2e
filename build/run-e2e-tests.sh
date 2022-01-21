d1797e8629e0151ffa13b752f91c742502fee614
Philip Wu <phwu@redhat.com>
2022-01-21 10:32:01 -0500

    Use release-2.2 branch for tests
    Signed-off-by: Philip Wu <phwu@redhat.com>

--- build/run-e2e-tests.sh
+++ build/run-e2e-tests.sh
@@ -58,7 +58,7 @@ setup_application_operator(){
     echo "Clone the application repo"
     echo
     if [ ! -d "multicloud-operators-application" ]; then
-        git clone https://github.com/stolostron/multicloud-operators-application.git
+        git clone -b release-2.2 https://github.com/stolostron/multicloud-operators-application.git
     fi
 
     kubectl apply -f multicloud-operators-application/deploy/crds
@@ -68,7 +68,7 @@ setup_channel_operator(){
     echo "Clone the channel repo"
     echo
     if [ ! -d "multicloud-operators-channel" ]; then
-        git clone https://github.com/stolostron/multicloud-operators-channel.git
+        git clone -b release-2.2 https://github.com/stolostron/multicloud-operators-channel.git
     fi
 
 	sed -i -e "s|image: .*$|image: quay.io/stolostron/multicluster-operators-channel:community-${COMPONENT_VERSION}|" multicloud-operators-channel/deploy/standalone/operator.yaml
@@ -87,7 +87,7 @@ setup_subscription_operator(){
     echo "Clone the subscription repo"
     echo
     if [ ! -d "multicloud-operators-subscription" ]; then
-        git clone https://github.com/stolostron/multicloud-operators-subscription.git
+        git clone -b release-2.2 https://github.com/stolostron/multicloud-operators-subscription.git
     fi
 
     kubectl apply -f multicloud-operators-subscription/deploy/common
@@ -109,7 +109,7 @@ setup_placementrule_operator(){
     echo "Clone the placementrule repo"
     echo
     if [ ! -d "multicloud-operators-placementrule" ]; then
-        git clone https://github.com/stolostron/multicloud-operators-placementrule.git
+        git clone -b release-2.2 https://github.com/stolostron/multicloud-operators-placementrule.git
     fi
 
     kubectl apply -f https://raw.githubusercontent.com/stolostron/multicloud-operators-placementrule/master/deploy/crds/apps.open-cluster-management.io_placementrules_crd.yaml
@@ -119,7 +119,7 @@ setup_helmrelease_operator(){
     echo "Clone the helmrelease repo"
     echo
     if [ ! -d "multicloud-operators-subscription-release" ]; then
-        git clone https://github.com/stolostron/multicloud-operators-subscription-release.git
+        git clone -b release-2.2 https://github.com/stolostron/multicloud-operators-subscription-release.git
     fi
 
     sed -i -e "s|image: .*$|image: quay.io/stolostron/multicluster-operators-subscription-release:community-$COMPONENT_VERSION|" multicloud-operators-subscription-release/deploy/operator.yaml
